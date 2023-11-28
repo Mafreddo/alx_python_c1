79178b8adfdc2779fd25796eef8d2074af00abb1
Mafreddo <sello.mathekga334@gmail.com>
2023-11-28 17:08:02 +0200

    alx_python

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,1 +1,4 @@
 print("Programming is like building a multilingual puzzle")
+print (guillaume@ubuntu:~/py/$ python3 0-print.py 
+Programming is like building a multilingual puzzle
+guillaume@ubuntu:~/py/$)

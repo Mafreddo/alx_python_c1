a1703ed8b0d2571c940a035163b1196525b9ca2c
Mafreddo <sello.mathekga334@gmail.com>
2023-11-30 18:58:25 +0200

    python-hello_world

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,7 +1,1 @@
 print("Programming is like building a multilingual puzzle")
-#!/usr/bin/python3
-word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print("First 3 letters: {}".format(Hol))
-print("Last 2 letters: {}".format(on))
-print("Middle word: {}".format(olberto))

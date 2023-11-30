1a3ecdbc5da45e1edc54f4ce4ce1a90a4eb6c71d
Mafreddo <sello.mathekga334@gmail.com>
2023-11-30 10:59:26 +0200

    pythin-hello_world

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,4 +1,7 @@
 print("Programming is like building a multilingual puzzle")
-print (guillaume@ubuntu:~/py/$ python3 0-print.py 
-Programming is like building a multilingual puzzle
-guillaume@ubuntu:~/py/$)
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print("First 3 letters: {}".format(Hol))
+print("Last 2 letters: {}".format(on))
+print("Middle word: {}".format(olberto))

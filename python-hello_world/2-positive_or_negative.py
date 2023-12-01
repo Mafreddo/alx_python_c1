c5097a83a45f961a4bc75503b9e678abc0ead132
Mafreddo <sello.mathekga334@gmail.com>
2023-12-01 20:39:55 +0200

    python-hello_world

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -2,3 +2,10 @@
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
+
+if number> 0:
+    print("is positive")
+if number== 0:
+    print("is zero")
+if number< 0:
+    print("is negitive")
--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 
-str("last digit of" (number)) = (10000)
+str("last digit of (number)") == (10000)
 if number>5:
     print("and is greater than 5")
-if number=0:
+if number==0:
     print("and is 0")
-if number<6>0:
+if number<6 and not 0:
     print("and less than 6 and not 0")
